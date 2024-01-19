2f9ecaab2b683f1581780421b9e01c3de52190f6
MRFish <34245941+M-Ryba@users.noreply.github.com>
2024-01-19 22:06:04 +0100

    Update test_main.py

--- src/test_main.py
+++ src/test_main.py
@@ -1,1 +1,13 @@
+import main
 
+
+def test_is_adult():
+    age = 18
+    result = main.is_adult(age)
+    assert result
+    assert main.is_adult(20)
+
+
+def test_is_not_adult():
+    assert not main.is_adult(17)
+    assert not main.is_adult(19)
